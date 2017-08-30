7621cbe4ff12814fc8c63d0a3b613f864a7c5418
davidmgudeman <davidmgudeman@gmail.com>
2017-08-29 17:13:05 -0700

    added the http request with axios library

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,20 @@
 
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+
+  componentWillMount() {
+    // ASYNC HTTP Request to get albums from the API.
+    // eslint-disable-next-line
+    // fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    //   .then((response) => response.json())
+    //   .then((responseData) => this.setState({ albums: responseData }));
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
+  }
+  
   render() {
     return (
       <View>
