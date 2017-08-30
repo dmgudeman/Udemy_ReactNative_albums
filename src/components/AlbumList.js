a67af8e75c9c36d02685aa22a2f8c5218f57da86
davidmgudeman <davidmgudeman@gmail.com>
2017-08-30 09:02:32 -0700

    coded AlbumDetail component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,24 +1,38 @@
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
-
+  // Step 1: initial, empty state
+  state = { albums: [] };
+  
   componentWillMount() {
     // ASYNC HTTP Request to get albums from the API.
-    // eslint-disable-next-line
+    
     // fetch('https://rallycoding.herokuapp.com/api/music_albums')
     //   .then((response) => response.json())
     //   .then((responseData) => this.setState({ albums: responseData }));
+
+    // the setState method is provided by the Component class.
+
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => console.log(response));
+    // Step 2: set state
+    .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+  return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album} />
+   );
   }
   
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>Album List!!!!!!</Text>
+        { this.renderAlbums() }
       </View>
     );
   }
